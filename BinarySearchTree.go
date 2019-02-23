eb5b94c74fb72515ce35132ac566e96e79c6c983
jacobtie <jacobtie@gmail.com>
2019-02-23 17:26:44 -0500

    Added pre and post order traversals with refactoring of in order

--- BinarySearchTree.go
+++ BinarySearchTree.go
@@ -33,10 +33,29 @@ func (bst *BinarySearchTree) insert_helper(subRoot *BinaryTreeNode, node *Binary
 	}
 }
 
-func (bst *BinarySearchTree) inOrderTraversal() []int {
+func (bst *BinarySearchTree) traversal(t string) []int {
 	results := make([]int, 0, bst.numItems)
-	bst.inOrderTraversal_helper(bst.root, &results)
+	switch t {
+	case "in":
+		bst.inOrderTraversal_helper(bst.root, &results)
+	case "pre":
+		bst.preOrderTraversal_helper(bst.root, &results)
+	case "post":
+		bst.postOrderTraversal_helper(bst.root, &results)
+	}
 	return results
+}
+
+func (bst *BinarySearchTree) inOrderTraversal() []int {
+	return bst.traversal("in")
+}
+
+func (bst *BinarySearchTree) preOrderTraversal() []int {
+	return bst.traversal("pre")
+}
+
+func (bst *BinarySearchTree) postOrderTraversal() []int {
+	return bst.traversal("post")
 }
 
 func (bst *BinarySearchTree) inOrderTraversal_helper(subRoot *BinaryTreeNode, results *[]int) {
@@ -46,4 +65,22 @@ func (bst *BinarySearchTree) inOrderTraversal_helper(subRoot *BinaryTreeNode, re
 	bst.inOrderTraversal_helper(subRoot.Left, results)
 	*results = append(*results, subRoot.Element)
 	bst.inOrderTraversal_helper(subRoot.Right, results)
+}
+
+func (bst *BinarySearchTree) preOrderTraversal_helper(subRoot *BinaryTreeNode, results *[]int) {
+	if subRoot == nil {
+		return
+	}
+	*results = append(*results, subRoot.Element)
+	bst.preOrderTraversal_helper(subRoot.Left, results)
+	bst.preOrderTraversal_helper(subRoot.Right, results)
+}
+
+func (bst *BinarySearchTree) postOrderTraversal_helper(subRoot *BinaryTreeNode, results *[]int) {
+	if subRoot == nil {
+		return
+	}
+	bst.postOrderTraversal_helper(subRoot.Left, results)
+	bst.postOrderTraversal_helper(subRoot.Right, results)
+	*results = append(*results, subRoot.Element)
 }
--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{6, 4, 65, 2, 9, 1, 8}
+	arr := []int{28, 99, 42, 10, 11, 2, 53, 85}
 	bst := &BinarySearchTree{nil, 0}
 	bst.insert(arr...)
-	fmt.Println(bst)
 	fmt.Println(bst.inOrderTraversal())
+	fmt.Println(bst.preOrderTraversal())
+	fmt.Println(bst.postOrderTraversal())
 }
