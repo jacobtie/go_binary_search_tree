844d2610af1b2d3a8cc69f7442a8c873408031fd
jacobtie <jacobtie@gmail.com>
2019-02-23 23:20:37 -0500

    Replaced BST test with simple menu interface

--- main.go
+++ main.go
@@ -1,12 +1,63 @@
 package main
 
-import "fmt"
+import(
+	"fmt"
+)
 
 func main() {
-	arr := []int{28, 99, 42, 10, 11, 2, 53, 85}
-	bst := &BinarySearchTree{nil, 0}
-	bst.insert(arr...)
-	fmt.Println(bst.inOrderTraversal())
-	fmt.Println(bst.preOrderTraversal())
-	fmt.Println(bst.postOrderTraversal())
+	runMenu()
+}
+
+func runMenu() {
+	var num int
+	bst := &BinarySearchTree{}
+
+	for num != 5 {
+		fmt.Println("\nChoose an option from the following list")
+		fmt.Println("1. Insert Numbers")
+		fmt.Println("2. Run In Order Traveral")
+		fmt.Println("3. Run Pre Order Traveral")
+		fmt.Println("4. Run Post Order Traveral")
+		fmt.Println("5. Quit\n")
+		
+		fmt.Scanf("%d", &num)
+
+		fmt.Print("\n")
+
+		switch num {
+		case 1:
+			// bst.insert(getNum())
+			bst.insert(insertNums()...)
+		case 2:
+			fmt.Println(bst.inOrderTraversal())
+		case 3:
+			fmt.Println(bst.preOrderTraversal())
+		case 4:
+			fmt.Println(bst.postOrderTraversal())
+		}
+	}
+}
+
+func insertNums() []int {
+	num := -1
+	var arr []int
+
+	for num != 0 {
+		fmt.Println("Enter 1 to enter a number. Enter 0 to stop entering numbers.")
+		fmt.Scanf("%d", &num)
+		if num == 1 {
+			arr = append(arr, getNum())
+		}
+	}
+
+	return arr
+}
+
+func getNum() int {
+	var num int
+
+	fmt.Print("Please enter an integer: ")
+	fmt.Scanf("%d", &num)
+	
+	return num
 }
